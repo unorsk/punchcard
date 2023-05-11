aea329e58df709a474fd8b39e54e4c798f888d58
unorsk <25188+unorsk@users.noreply.github.com>
2023-05-11 10:43:54 +0200

    add static files and better styles

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 import Web.Scotty (scotty, get, param, html)
-import Lucid.Html5 (div_, class_)
+import Lucid.Html5 (div_, class_, body_, link_, rel_, href_)
 import Lucid (Html, renderText, toHtml)
-import Data.Text.Internal.Lazy (Text)
 import Data.Time.Calendar
+import Data.Foldable (fold)
+import Web.Scotty.Trans (middleware)
+import Network.Wai.Middleware.Static (staticPolicy, noDots, (>->), addBase)
 
 
 lastMonday :: Day
@@ -14,18 +16,34 @@ yearInWeeks :: [Day]
 yearInWeeks = [lastMonday .. addDays 371 lastMonday]
 
 printDay :: Day -> Html ()
-printDay day = div_ [class_ "punchcard-day"] $ toHtml $ show day
+printDay day = div_ [class_ "punchcard-day"] $ toHtml ("" :: String)
 
+weekDaysHeaders :: Html()
+weekDaysHeaders =
+  div_ [class_ "week"]
+    (fold $ map div_ ["Ma", "Ti", "On", "To", "Fr", "Lø", "Sø"])
 
-renderPunchCard :: Text
-renderPunchCard = 
+splitPeriodsIntoWeeks :: [Html ()] -> Html()
+splitPeriodsIntoWeeks periods =
+  case periods of
+    [] -> div_ [class_ "week"] ""
+    p -> let week = (div_ [class_ "week"] $ fold (take 7 p)) in
+          week <> fold [splitPeriodsIntoWeeks (drop 7 p)]
+
+renderStyles :: Html ()
+renderStyles = link_ [rel_ "stylesheet", href_ "styles.css" ]
+
+renderPunchCard :: Html ()
+renderPunchCard =
   let periods = map printDay yearInWeeks in
-  renderText (div_ [class_ "punchcard"] (foldr (<>) (div_ "") periods))
+  -- renderText (div_ [class_ "punchcard"] (fold periods))
+  body_  (div_ [class_ "punchcard"] (weekDaysHeaders <> splitPeriodsIntoWeeks periods))
 
 main :: IO ()
 main = scotty 3000 $ do
+  middleware $ staticPolicy (noDots >-> addBase "static")
   get "/hello/:hello" $ do
       hello <- param "hello"
       html $ mconcat ["<h1>", hello, "</h1>"]
   get "/" $ do
-      html renderPunchCard
+      html $ renderText (renderPunchCard <> renderStyles)
